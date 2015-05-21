a34a188137a4558b889dc7ebe59a7f96433bb866
Jonathan Moerman <jonathanmoerman@gmail.com>
2015-05-21 17:20:19 +0200

    Legal stuff

--- app/src/main/java/tk/slicesofcheese/the_best_application_ever/View/TouchOverlay.java
+++ app/src/main/java/tk/slicesofcheese/the_best_application_ever/View/TouchOverlay.java
@@ -1,3 +1,20 @@
+/* Copyright 2015 Jan Potma, Jonathan Moerman, Mathis Sackers, Tom Nijholt
+*
+* This file is part of a:Dventure.
+*
+* a:Dventure is free software: you can redistribute it
+* and/or modify it under the terms of the GNU General Public License as
+* published by the Free Software Foundation, either version 2 of the
+* License, or (at your option) any later version.
+*
+* a:Dventure is distributed in the hope that it will be
+* useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+* Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with a:Dventure. If not, see http://www.gnu.org/licenses/.
+*/
 package tk.slicesofcheese.the_best_application_ever.View;
 
 import android.content.Context;
@@ -20,7 +37,7 @@ import tk.slicesofcheese.the_best_application_ever.Model.Entities.Wall;
 import tk.slicesofcheese.the_best_application_ever.Model.Level;
 
 /**
- * Created by jonathan on 30-4-15.
+ * Shows the pressed area.
  */
 public class TouchOverlay extends View implements Observer {
 
