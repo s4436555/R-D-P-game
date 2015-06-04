e3d2844e062435c54e3b3599741e5856658beac2
s4436555 <j.moerman@student.ru.nl>
2015-06-04 11:27:10 +0200

    Added some headers

--- app/src/main/java/free/lunch/aDventure/Model/LevelGenerator.java
+++ app/src/main/java/free/lunch/aDventure/Model/LevelGenerator.java
@@ -1,3 +1,20 @@
+/* Copyright 2015 Jan Potma, Jonathan Moerman, Mathis Sackers, Tom Nijholt
+*
+* This file is part of a:Dventure.
+*
+* a:Dventure is free software: you can redistribute it
+* and/or modify it under the terms of the GNU General Public License as
+* published by the Free Software Foundation, either version 2 of the
+* License, or (at your option) any later version.
+*
+* a:Dventure is distributed in the hope that it will be
+* useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+* Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with a:Dventure. If not, see http://www.gnu.org/licenses/.
+*/
 package free.lunch.aDventure.Model;
 
 import free.lunch.aDventure.Model.Entities.Enemies.Dragon;
