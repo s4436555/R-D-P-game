b44f17262c26a43044d27bf271a95c6d8a3d9924
slicesofcheese <tomnijholt@home.nl>
2015-06-09 20:14:09 +0200

    Highscores ERROR solved

--- app/src/main/java/free/lunch/aDventure/HighScoreActivity.java
+++ app/src/main/java/free/lunch/aDventure/HighScoreActivity.java
@@ -40,32 +40,33 @@ public class HighScoreActivity extends Activity {
             TextView desc = (TextView) findViewById(descID);
 
             //TODO:Load highscore names and scores
-
-            switch (i){
-                case 0:
-                    player.setText(outputScores[0]);
-                    desc.setText("is dominating the leaderboard with " +outputScores[2]+  " points\nDate: " +outputScores[1]);
-                    break;
-                case 1:
-                    player.setText(outputScores[0]);
-                    desc.setText("is the runner up with " +outputScores[2]+ " points\nDate: " +outputScores[1]);
-                    break;
-                case 2:
-                    player.setText(outputScores[0]);
-                    desc.setText("spent quite some time getting " +outputScores[2]+ " points\nDate: " +outputScores[1]);
-                    break;
-                case 8:
-                    player.setText(outputScores[0]);
-                    desc.setText("is so bad he only got " +outputScores[2]+  " points\nDate: " +outputScores[1]);
-                    break;
-                case 9:
-                    player.setText(outputScores[0]);
-                    desc.setText("nearly didn't make it on here with his " +outputScores[2]+  " points\nDate: " +outputScores[1]);
-                    break;
-                default:
-                    player.setText(outputScores[0]);
-                    desc.setText("doesn't get an individual text with his " +outputScores[2]+ " points\nDate: " +outputScores[1]);
-                    break;
+            if (outputScores.length == 3) {
+                switch (i){
+                    case 0:
+                        player.setText(outputScores[0]);
+                        desc.setText("is dominating the leaderboard with " +outputScores[2]+  " points\nDate: " +outputScores[1]);
+                        break;
+                    case 1:
+                        player.setText(outputScores[0]);
+                        desc.setText("is the runner up with " +outputScores[2]+ " points\nDate: " +outputScores[1]);
+                        break;
+                    case 2:
+                        player.setText(outputScores[0]);
+                        desc.setText("spent quite some time getting " +outputScores[2]+ " points\nDate: " +outputScores[1]);
+                        break;
+                    case 8:
+                        player.setText(outputScores[0]);
+                        desc.setText("is so bad he only got " +outputScores[2]+  " points\nDate: " +outputScores[1]);
+                        break;
+                    case 9:
+                        player.setText(outputScores[0]);
+                        desc.setText("nearly didn't make it on here with his " +outputScores[2]+  " points\nDate: " +outputScores[1]);
+                        break;
+                    default:
+                        player.setText(outputScores[0]);
+                        desc.setText("doesn't get an individual text with his " +outputScores[2]+ " points\nDate: " +outputScores[1]);
+                        break;
+                }
             }
         }
     }
