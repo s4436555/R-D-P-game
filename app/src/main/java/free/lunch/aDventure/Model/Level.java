d0306a0727156e41ab8be303083c75c51fe4d6c6
OsthatoAlfakyn <jan.potma@gmail.com>
2015-06-10 19:31:17 +0200

    Spawnbug fixed again

--- app/src/main/java/free/lunch/aDventure/Model/Level.java
+++ app/src/main/java/free/lunch/aDventure/Model/Level.java
@@ -105,7 +105,9 @@ public class Level implements Serializable{
         if (player != null) {
             for (int pX = player.getX()-1; pX <= player.getX()+1; pX++) {
                 for (int pY = player.getY() - 1; pY <= player.getY() + 1; pY++) {
-                    if (isValid(pX, pY) && cells[pX][pY] != null) return false;
+                    if (isValid(pX, pY) {
+                        if (cells[pX][pY] != null) return false;
+                    }
                 }
             }
         }
